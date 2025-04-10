bdda916e97f6400304f5cf6401d0f8a83f81710b
Saurabh2627 <saurabhsunil26@gmail.com>
2025-04-10 12:32:36 -0400

    commit

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
 @app.route('/')
 def main():
-    return "Welcome to Smith Final Test API Server"
+    return "Welcome to Gopale Final Test API Server"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
